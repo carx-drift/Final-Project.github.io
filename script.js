ba0dd3112f51af6c44c09b9ed3775ca6167b2cc9
carx-drift <siraitjulio855@gmail.com>
2025-11-10 19:51:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const menuData = {
+    pizza:[
+        name:"pizza margherita",
+        desc: "tomatensauce, mozarella, basillikum, oliven, kapern",
+        price: "11,20€",
+        img: ""
+    }
+    {
+        name: "pizza tonno",
+        desc: "tomatensauce, oregano, sardellen, knoblauch, oliven",
+        price: "9.50€",
+        img:""
+    }
+    {
+        name:"pizza salame",
+        desc: "tomatensauce, mozarella, salami, oliven, kapern",
+        price: "9,40€"
+        img: ""
+    }
+    ],
+    pasta:[
+        {
+            name: "spaghetti bolognese",
+            desc: "mit hausgemachter tomaten-Hackfleischsoße",
+            price: "8,90€",
+            img: ""
+        }
+        {
+            name: "penne al pesto und pinienkernen",
+            desc: "mit basilikum-Pesto und pinienkernen",
+            price: "8,50€",
+            img: ""
+        }
+    ],
+    meat:[
+        {
+            name:"rinderfilet",
+            desc:'mit rotweinsauce und gemüse'
+            price: "17,90€",
+            img:""
+        }
+    ],
+    fish: [
+        {
+            name: "gegrillter lachs",
+            desc: "mit zitronenbutter und kräutern",
+            price: "15,90€"
+            img:"                           "
+        }
+    ]
+    
